f41be968c0165dabc2cc623eaf21746e7bf36258
marco <marcomoura947@gmail.com>
2026-04-10 09:58:03 -0300

    first commit

---
+++ src/main/java/br/dev/marco/FastFuriosFood1/FastFuriosFood1Application.java
@@ -0,0 +1,13 @@
+package br.dev.marco.FastFuriosFood1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFuriosFood1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFuriosFood1Application.class, args);
+	}
+
+}
